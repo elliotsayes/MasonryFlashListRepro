ccd6379ae655914f6be13fe1d470388d977acd28
Elliot Sayes <elliot@sayes.cloud>
2022-09-27 12:13:53 +1300

    Add MasonryFlashList (triggers warning)

--- src/screens/development/NoteListScreen.tsx
+++ src/screens/development/NoteListScreen.tsx
@@ -1,8 +1,23 @@
+import { FlashList, MasonryFlashList } from "@shopify/flash-list"
 import { Text } from "react-native"
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
 
 const NoteListScreen = () => {
   return (
-    <Text>NoteListScreen</Text>
+    <MasonryFlashList
+      data={DATA}
+      numColumns={2}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={200}
+    />
   )
 }
 
